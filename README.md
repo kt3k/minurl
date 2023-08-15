a6d203c60bc4d98d144f287042974fc729054006
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-15 16:44:45 +0900

    modify

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # minurl
 
-Minimalistic URL Shortner for [Deno](https://deno.com/runtime) +
-[KV](https://deno.com/kv)
+Minimalistic URL Shortner for [Deno Deploy](https://deno.com/deploy) using
+[Deno KV](https://deno.com/kv)
 
 - No external dependencies
 - Uses Deno KV as data backend
-- Written in less than 30 lines
+- 64-bit space
 
 # License
 
--- main.ts
+++ main.ts
@@ -1,27 +1,20 @@
 // Copyright 2023 Yoshiya Hinosawa. All rights reserved. MIT license.
-
 const kv = await Deno.openKv();
-
 Deno.serve(async (req) => {
   const { origin, pathname } = new URL(req.url);
-
   if (req.method === "POST") {
     const url = new URL(await req.text());
-    const slug = crypto.getRandomValues(new Uint32Array(1))[0].toString(36);
-    const key = ["urls", slug];
+    const path = crypto.getRandomValues(new BigUint64Array(1))[0].toString(36);
+    const key = ["urls", "/" + path];
     await kv.atomic().check({ key, versionstamp: null }).set(key, url.href)
       .commit();
-    return new Response(`Created ${origin}/${slug}\n`);
+    return new Response(`Created ${origin}/${path}\n`);
   }
-
   if (pathname === "/") {
     return new Response(
       `Welcome to URL Shortner! Create a short url with the command:\n\n curl -d https://example.com ${origin}`,
     );
   }
-
-  const { value } = await kv.get<string>(["urls", pathname.slice(1)]);
-  return value
-    ? Response.redirect(value, 307)
-    : new Response("Not Found", { status: 404 });
+  const v = (await kv.get<string>(["urls", pathname])).value;
+  return v ? Response.redirect(v) : new Response("404", { status: 404 });
 });
