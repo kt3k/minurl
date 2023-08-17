ea127ac02e16ebf3f9d759e1282a2d5477cdb9bf
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-17 14:17:41 +0900

    simplify kv key

--- main.ts
+++ main.ts
@@ -4,13 +4,12 @@ Deno.serve(async (req) => {
   const { origin, pathname } = new URL(req.url);
   if (req.method === "POST") {
     const url = new URL(await req.text()).href;
-    const path = crypto.getRandomValues(new BigUint64Array(1))[0].toString(36);
-    const key = ["urls", "/" + path];
+    const key = [crypto.getRandomValues(new BigUint64Array(1))[0].toString(36)];
     await kv.atomic().check({ key, versionstamp: null }).set(key, url).commit();
-    return new Response(`Created ${origin}/${path}\n`);
+    return new Response(`Created ${origin}/${key[0]}\n`);
   } else if (pathname === "/") {
     return new Response(msg + `curl -d https://example.com ${origin}`);
   }
-  const v = (await kv.get<string>(["urls", pathname])).value;
+  const v = (await kv.get<string>([pathname.slice(1)])).value;
   return v ? Response.redirect(v) : new Response("Not Found", { status: 404 });
 });
