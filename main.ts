700542a42bf87a292a31673b345a5855d9a05ef3
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-17 14:28:04 +0900

    chore: clean up

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
 const kv = await Deno.openKv();
-const msg = "Welcome! Shorten the url with the command:\n\n  ";
-Deno.serve(async (req) => {
-  const { origin, pathname } = new URL(req.url);
-  if (req.method === "POST") {
-    const url = new URL(await req.text()).href;
+
+Deno.serve(async ({ method, text, url }) => {
+  const { origin, pathname } = new URL(url);
+
+  if (method === "POST") {
     const key = [crypto.getRandomValues(new BigUint64Array(1))[0].toString(36)];
-    await kv.atomic().check({ key, versionstamp: null }).set(key, url).commit();
+    const val = new URL(await text()).href;
+
+    await kv.atomic().check({ key, versionstamp: null }).set(key, val).commit();
     return new Response(`Created ${origin}/${key[0]}\n`);
-  } else if (pathname === "/") {
-    return new Response(msg + `curl -d https://example.com ${origin}`);
   }
+
+  if (pathname === "/") {
+    const msg = "Welcome! Shorten the url with the command:\n\n  " +
+      `curl -d https://example.com ${origin}`;
+    return new Response(msg);
+  }
+
   const v = (await kv.get<string>([pathname.slice(1)])).value;
   return v ? Response.redirect(v) : new Response("Not Found", { status: 404 });
 });
